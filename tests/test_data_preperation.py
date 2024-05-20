33c750ed96a5e096d69784a23033af9abb41fdfe
Dhracyr <moritz@goemmel.com>
2024-05-20 15:22:13 +0200

    Add h5py downloader for the one-million-songs-dataset and update requirements.txt

--- tests/test_data_preperation.py
+++ tests/test_data_preperation.py
@@ -1,5 +1,5 @@
 from data_preperation import dataset_snapshot
 
-dataset_as_snapshots = dataset_snapshot.process_dataset_multithreaded("../datasets/midi_test_files", 1)
+dataset_as_snapshots = dataset_snapshot.process_dataset("../datasets/midi_test_files", 1)
 
 dataset_snapshot.print_dataset(dataset_as_snapshots)
