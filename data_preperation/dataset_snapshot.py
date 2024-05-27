31bac62ad3f5034c35f38688e99033a6273e4799
Dhracyr <moritz@goemmel.com>
2024-05-27 12:21:34 +0200

    Add export_harmony_melody_to_csv and implement it into test_data_preperation.py

--- data_preperation/dataset_snapshot.py
+++ data_preperation/dataset_snapshot.py
@@ -4,6 +4,7 @@ import os
 import fnmatch
 
 import numpy as np
+import pandas as pd
 
 # for progress bar:
 from tqdm import tqdm
@@ -154,6 +155,21 @@ def extract_melody_and_harmony(dataset_as_snapshots):
 
     return melody_harmony_dataset
 
+
+def export_melody_harmony_to_csv(melody_harmony_dataset, output_dir):
+    for filename, melody_snapshots, harmony_snapshots in melody_harmony_dataset:
+        # Prepare filenames for CSV
+        base_filename = filename.split('/')[-1].split('.')[0]
+        melody_filename = f"{output_dir}/{base_filename}_melody.csv"
+        harmony_filename = f"{output_dir}/{base_filename}_harmony.csv"
+
+        # Convert to DataFrames
+        melody_df = pd.DataFrame(melody_snapshots)
+        harmony_df = pd.DataFrame(harmony_snapshots)
+
+        # Export to CSV
+        melody_df.to_csv(melody_filename, index=False)
+        harmony_df.to_csv(harmony_filename, index=False)
 
 def filter_piano_range(dataset_as_snapshots):
     filtered_dataset = []
