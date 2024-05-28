ab9136ae53ed9bbe7c0621d0defb73b2c48eb21f
Dhracyr <moritz@goemmel.com>
2024-05-28 18:43:50 +0200

    Add small fixes and refactoring

--- tests/test_data_preparation.py
+++ tests/test_data_preparation.py
@@ -1,7 +1,10 @@
 from data_preperation import dataset_snapshot
 from data_preperation.dataset_snapshot import *
 
-dataset_as_snapshots = dataset_snapshot.process_dataset("../datasets/maestro-split-v3/small_batch_lstm/mid_split", .1)
+
+INTERVAL = 0.05
+dataset_as_snapshots = dataset_snapshot.process_dataset("../datasets/maestro-split-v3/small_batch_lstm/mid_split",
+                                                        INTERVAL)
 
 filtered_dataset = filter_piano_range(dataset_as_snapshots)
 
