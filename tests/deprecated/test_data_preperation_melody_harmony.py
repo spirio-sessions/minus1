0e979f6d62a6d615b35818b8186580602dd5986c
Dhracyr <moritz@goemmel.com>
2024-05-28 18:13:18 +0200

    Add deprecated directory for failed attempts

---
+++ tests/deprecated/test_data_preperation_melody_harmony.py
@@ -0,0 +1,11 @@
+from data_preperation import dataset_snapshot
+from data_preperation.dataset_snapshot import *
+
+dataset_as_snapshots = dataset_snapshot.process_dataset("../datasets/jazz_mlready_dataset/small_batch/mid/", .1)
+
+filtered_dataset = filter_piano_range(dataset_as_snapshots)
+
+melody_harmony_dataset = extract_melody_and_harmony(filtered_dataset)
+print_melody_harmony_dataset(melody_harmony_dataset)
+export_melody_harmony_to_csv(melody_harmony_dataset, "../datasets/jazz_mlready_dataset/small_batch/csv")
+
