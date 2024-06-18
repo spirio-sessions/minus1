25b63df3ad6bd1cdcea580b564d0583fb13afcd4
Dhracyr <moritz@goemmel.com>
2024-06-18 12:40:12 +0200

    Add deprecation to aubio implementation, add directory for 13keys realtime implementation

---
+++ pipeline_lstm_13keys/07_real_time/test_print_real_time_stats.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+from lstm_training.print_results import print_results, printHeatmap
+
+"""
+This script takes the predicted harmony and both originals for statistik and inference.
+The print includes:
+    1. Mean-Square-Error
+    2. Compare with baseline
+    3. Scale of data with min, max, mean, std
+    4. Plot of predicted_harmony
+    5. Heatmap for key-probability in snapshot
+    6. Heatmap for which key is getting played at what time
+"""
+
+# Load predicted data from realtime pitch
+predicted_harmony = pd.read_csv('predicted_data.csv').values
+original_melody = pd.read_csv('pitch_data.csv').values
+original_harmony = pd.read_csv('predicted_data.csv').values
+
+print_results(predicted_harmony, original_melody, original_harmony)
+printHeatmap(predicted_harmony, 0, -28, 38)
---
+++ pipeline_lstm_13keys/07_real_time/test_real_time_processing.py
@@ -0,0 +1,27 @@
+from lstm_training.load_data_from_csv import load_data_from_csv
+from lstm_training.load_lstm_model import load_lstm_model
+from lstm_training.process_audio_stream import *
+from lstm_training.process_audio_stream_matplotlib import *
+
+
+# Load melody and harmony from csv
+melody, harmony = load_data_from_csv('../03_lstm_training/csv')
+
+
+# Parameters
+num_features = 88
+sequence_length = 5
+
+# Check if cuda is available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using {device} as device.')
+
+model, parameters = load_lstm_model('../04_finished_model/models', 'lstm_03', device, True)
+
+# Unpack parameters
+input_size, hidden_size, num_layers, output_size, learning_rate, num_epochs, batch_size = parameters
+
+# Call the function to start processing
+process_audio_stream(model, device, sequence_length, num_features)
+
+# TODO: Add Doc-String when functional
