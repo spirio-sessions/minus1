5c5933f569280cfd34274b9a89be145adc6cb414
falaxdb <Falaxdb@proton.me>
2024-07-03 21:37:06 +0200

    Try sequence prediction with single sequence

---
+++ transformer_decoder_training/dataset_transformer/dataset_2.py
@@ -0,0 +1,47 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
+class AdvancedPianoDataset(Dataset):
+    def __init__(self, data: list, seq_length: int, stride: int, start_token: np.ndarray):
+        """
+        :param data: A list of Songs. Each song is a list. A song has 2 numpy arrays: First the right hand then the left.
+        :param seq_length: Length of sequence chunks.
+        :param stride: How far the start of the second sequence should be from the first.
+        :param start_token: The token to add at the beginning of each src and target chunk.
+        """
+
+        self.data = []
+        for song in data:
+            if len(song[0]) != len(song[1]):
+                raise ValueError("Numpy arrays for Hands must have the same length")
+
+            song = np.array(song)
+            # Concatenate right and left hand (left hand first, index wise), so we combine right and left in one snapshot
+            song = np.concatenate((song[1], song[0]), axis=1)
+            self.data.append(song)
+
+        self.seq_length = seq_length
+        self.stride = stride
+        self.start_token = start_token
+        self.sequences = self._create_sequences()
+
+    def _create_sequences(self):
+        sequences = []
+        for song in self.data:
+            for i in range(0, len(song) - self.seq_length, self.stride):
+                seq = song[i:i + self.seq_length]
+                if len(seq) == self.seq_length:  # Ensure sequence is of required length
+                    # Add the start token at the beginning of the sequence
+                    seq_with_start = np.vstack((self.start_token, seq))
+                    sequences.append(seq_with_start)
+        return sequences
+
+    def __len__(self):
+        return len(self.sequences)
+
+    def __getitem__(self, idx):
+        seq = self.sequences[idx]
+        seq_tensor = torch.tensor(seq, dtype=torch.float32)
+        return seq_tensor
---
+++ transformer_decoder_training/inference/inference_2.py
@@ -0,0 +1,56 @@
+import torch
+
+
+def inference(model, context_sequence, true_continuing_sequence, threshold, pad_token, device):
+    generated_tokens = []
+    generated_harmony = []
+
+    # Clone sequence so we don't change the original input
+    input_seq = context_sequence.clone()
+
+    with torch.no_grad():
+        for i in range(len(true_continuing_sequence)):
+            input_seq = input_seq.to(device)
+
+            # Model prediction for new token
+            data_pred = model(input_seq, pad_token)
+
+            if data_pred.shape[1] > input_seq.shape[1]:
+                print("Model might have generated more than one token")
+
+            # Get last token from output (should be the one new token)
+            next_token = data_pred[:, -1, :]
+            print("Next token shape:", next_token.shape)
+
+            # Change probability values to binary with threshold
+            next_token = (next_token >= threshold).float()
+
+            # Add token to list
+            generated_tokens.append(next_token)
+
+            # Replace generated right hand with right hand ground truth
+            # Determine the midpoint of the vector
+            vec_length = next_token.size(1)  # Should be size(1) for the correct dimension
+            midpoint = vec_length // 2
+
+            # Get the ground truth right hand:
+            ground_truth = true_continuing_sequence[0][i].to(device)
+            assert ground_truth.shape == next_token.shape  # Ensure same dimensions
+
+            # Get right hand truth
+            right_hand_truth = ground_truth[:, midpoint:]
+
+            # Split the original vector into two halves
+            first_half = next_token[:, :midpoint]
+            generated_harmony.append(first_half)
+
+            # Replace the second half with the ground truth vector
+            second_half = right_hand_truth
+
+            # Concatenate the first half of the original vector with the ground truth vector
+            next_token = torch.cat((first_half, second_half), dim=1)
+
+            # Append the new token to the sequence
+            input_seq = torch.cat((input_seq, next_token.unsqueeze(1)), dim=1)
+
+    return generated_tokens
